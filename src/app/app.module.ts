f03c6f31577600d3bcdd0f917d1227dbdd994051
wizardto1 <ph429@drexel.edu>
2020-04-15 22:12:59 -0400

    2 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { HomeComponent } from './home/home.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 import { LearningCenterComponent } from './learning-center/learning-center.component';
+import { VideoComponent } from './video/video.component';
+import { HomeEvalComponent } from './home-eval/home-eval.component';
 const appRoutes: Routes = [
-   { path: '', redirectTo : '/home', pathMatch:'full' },{path:'home', component: HomeComponent },{path:'learning',component:LearningCenterComponent}];
+   { path: '', redirectTo : '/home', pathMatch:'full' },{path:'home', component: HomeComponent },{path:'learning',component:LearningCenterComponent},{path:'video',component:VideoComponent},{path:'eval',component:HomeEvalComponent}];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(appRoutes) ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, TopBarComponent, BottomBarComponent, LearningCenterComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, TopBarComponent, BottomBarComponent, LearningCenterComponent, VideoComponent, HomeEvalComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
